b08cdc18bc3edb2d59e6cd866d9ec255641b240e
Thorsten Zoerner <me@thorsten-zoerner.com>
2022-02-21 00:46:10 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # tydids-p2p-http
 Tydids P2P HTTP(s) Relay and UI
+
+<a href="https://stromdao.de/" target="_blank" title="STROMDAO - Digital Energy Infrastructure"><img src="./static/stromdao.png" align="right" height="85px" hspace="30px" vspace="30px"></a>
+
+**A consensus driven decentralized data governance framework.**
+
+[![npm](https://img.shields.io/npm/dt/tydids-p2p-http.svg)](https://www.npmjs.com/package/tydids-p2p-http)
+[![npm](https://img.shields.io/npm/v/tydids-p2p-http.svg)](https://www.npmjs.com/package/tydids-p2p-http)
+[![CO2Offset](https://api.corrently.io/v2.0/ghgmanage/statusimg?host=tydids-p2p-http&svg=1)](https://co2offset.io/badge.html?host=tydids-p2p-http)
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/energychain/tydids-p2p-http)
---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var Gun = require('gun');
+const express = require('express')
+const app = express();
+
+var port = process.env.PORT || 8899;
+var fs = require('fs');
+const tydids = require("tydids-p2p");
+let _tmpwallet = tydids.ethers.Wallet.createRandom();
+let privateKey = _tmpwallet.privateKey;
+let settings = {};
+
+if(fs.existsSync('./.tydids.json')) {
+	settings = JSON.parse(fs.readFileSync('./.tydids.json'));
+	if(typeof settings.privateKey !== 'undefined') privateKey = settings.privateKey;
+} else {
+	settings = {
+		address:_tmpwallet.address,
+		privateKey: privateKey
+	}
+	fs.writeFileSync('./.tydids.json',JSON.stringify(settings));
+}
+
+// Serves up /index.html
+app.use(express.static('public'));
+
+const gun = Gun({
+	web: app, // Handles real-time requests and updates.
+  peers: ['https://webrtc.tydids.com/gun']
+});
+
+const main = async function() {
+		const ssi = await tydids.ssi(privateKey,gun);
+
+		app.get('/.well-known/tydids.json', (req, res) => {
+			res.json(
+				{
+					peers:['/gun'],
+					identity:ssi.identity
+				});
+		})
+
+		app.get('/tydids/retrieveVP', async (req, res) => {
+			if(typeof req.query.address !== 'undefined') {
+				let did = await ssi.retrieveVP(req.query.address);
+				res.json(did);
+			}
+		})
+
+		app.listen(port, function () {
+			console.log('\nApp listening on port', port);
+			console.log('Connect http://localhost:'+port+'/');
+		});
+}
+
+main();
