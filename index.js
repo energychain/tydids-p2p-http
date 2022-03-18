5af2781c7946875eca5c3de0564fdb5e4c35b25c
Thorsten Zoerner <me@thorsten-zoerner.com>
2022-03-18 17:00:23 +0100

    v1.2.1

--- index.js
+++ index.js
@@ -6,14 +6,35 @@ const port = 8888
 app.use(Gun.serve)
 
 const server = app.listen(port, () => {
-    console.log("Listening at: http://localhost://" + port)
+    console.log("Listening at: http://localhost:" + port)
 })
 
 const gun = Gun({web: server,file:"radata_http_"+new Date().getTime()})
+const tydids = require("tydids-p2p");
+const wallet = tydids.ethers.Wallet.createRandom();
+const privateKey = wallet.privateKey;
+
+let httpRelay = null;
+
 gun.get("relay").on(function(relay) {
 	for (const [key, value] of Object.entries(relay)) {
 		if(key.length>20) {
-  		gun.get(key).once(function(d) {},{wait:5000});
+  		gun.get(key).once(function(d) {});
+      console.log('Relay',key);
 		}
 	}
 });
+
+const runner = async function() {
+    const ssi = await tydids.ssi(privateKey,true,gun);
+    app.use('/json/', function (req, res, next) {
+      console.log(req.url.substr(1,42));
+      ssi.retrievePresentation(req.url.substr(1,42)).then(function(data) {
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.write(JSON.stringify(data))
+        res.end();
+      });
+    });
+}
+
+runner();
